77f35ea0ae06329c153b4d987754c0c1c1e8a9a4
dialarconr <diego.alarconrios@gmail.com>
2024-09-09 22:23:36 -0400

    asdf

--- app/page.js
+++ app/page.js
@@ -3,8 +3,8 @@ import reuben from './images/reuben.jpg';
 
 export default function Home() {
   return (
-    <div className="w-full">
-      <Image src={reuben} alt="Reuben" width={500} height={667} className="w-full h-auto object-center object-cover absolute" />
+    <div className="w-full min-h-screen">
+      <Image src={reuben} alt="Reuben" width={500} height={667} className="w-full h-full object-center object-cover absolute" />
     </div>
   );
 }
